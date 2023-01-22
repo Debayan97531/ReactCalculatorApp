090e147a32787f778d2ef0587c60c3f96da9435f
Debayan Kumar Ghosh <84261787+Debayan97531@users.noreply.github.com>
2023-01-22 20:51:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import "./styles.css";
+import {useState} from "react";
+
+export default function App() {
+
+  const [result,setResult]=useState("");
+  const handleClick=(e)=>{
+    setResult(result.concat(e.target.name));
+  }
+  const clear=()=>{
+    setResult("");
+  }
+  const backspace=()=>{
+    setResult(result.slice(0,result.length-1));
+  }
+  const calculate=()=>{
+    try{
+      setResult(eval(result)+" ");
+    }catch(error){
+      setResult("Error");
+    }
+  }
+
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Calculator</h1>
+
+        <div className="calculator">
+            <form>
+              <input type="text" value={result} id="screen" ></input>
+            </form>
+            <div className="keypad ">
+            {/* <table> */}
+                {/* <tr> */}
+                    <button name="1" onClick={handleClick} >1</button>
+                    <button name="2" onClick={handleClick}>2</button>
+                    <button name="3" onClick={handleClick}>3</button>
+                    <button name="+" onClick={handleClick}>+</button>
+                {/* </tr> */}
+                {/* <tr> */}
+                    <button name="4" onClick={handleClick}>4</button>
+                    <button name="5" onClick={handleClick}>5</button>
+                    <button name="6" onClick={handleClick}>6</button>
+                    <button name="-" onClick={handleClick}>-</button>
+                {/* </tr> */}
+                {/* <tr> */}
+                    <button name="7" onClick={handleClick}>7</button>
+                    <button name="8" onClick={handleClick}>8</button>
+                    <button name="9" onClick={handleClick}>9</button>
+                    <button name="*" onClick={handleClick}>*</button>
+                {/* </tr> */}
+                {/* <tr> */}
+                    <button name="0" onClick={handleClick}>0</button>
+                    <button name="." onClick={handleClick}>.</button>
+                    <button id="clear"  onClick={clear}>C</button>
+                    <button name="/" onClick={handleClick}>/</button>
+                {/* </tr> */}
+                {/* <tr> */}
+                    <button name="(" onClick={handleClick}>(</button>
+                    <button name=")" onClick={handleClick}>)</button>
+                    <button id="backspace"  onClick={backspace}>B</button>
+                    <button id="calculate"  onClick={calculate}>=</button>
+                {/* </tr> */}
+            {/* </table> */}
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
